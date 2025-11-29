2108a1434831f69c1be9f2229bab8a34c6510fa5
liu tt <liulytian2406@gmail.com>
2025-11-29 10:51:38 +0800

    add usemounted

--- app/widgets/index.tsx
+++ app/widgets/index.tsx
@@ -3,6 +3,8 @@
 import { useWidgetProps } from '@/app/hooks'
 import FlashCards from './flash-cards'
 import FlashCardsList from './flash-cards-list'
+import { Skeleton } from '../components/Skeleton'
+import useMounted from '../hooks/use-mount'
 
 const WIDGETS: Record<string, React.ComponentType> = {
   'flash-cards': FlashCards,
@@ -13,13 +15,14 @@ const DEFAULT_WIDGET = 'flash-cards'
 
 const WidgetSelector = () => {
   const widgetProps = useWidgetProps<{ type?: string }>()
+  const isMounted = useMounted()
 
   const widgetType = widgetProps?.type || DEFAULT_WIDGET
 
   const WidgetComponent = WIDGETS[widgetType]
 
-  if (!WidgetComponent) {
-    return <></>
+  if (!WidgetComponent || !isMounted) {
+    return <Skeleton />
   }
 
   return <WidgetComponent />
